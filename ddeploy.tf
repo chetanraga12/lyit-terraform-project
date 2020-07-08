db97bd292aa088cce6e697c2d4a5315dc2edc687
Dane C <64944405+danecode@users.noreply.github.com>
2020-07-08 23:07:08 +0100

    Update ddeploy.tf
    Syntax corrected

--- ddeploy.tf
+++ ddeploy.tf
@@ -1,6 +1,6 @@
 #Docker provider
 provider "docker" {
-  host = "tcp://127.0.0.1:2376/"
+	host = "tcp://127.0.0.1:2376/"
 }
 
 #Docker images
@@ -20,8 +20,12 @@ resource "docker_image" "phpmyadmin" {
 resource "docker_network" "private_network" {
   name = "dbnet"
   driver = "bridge"
-  options = "com.docker.network.bridge.name=dbnet"
-  subnet = "172.20.0.0/16"
+  options {
+  	"com.docker.network.bridge.name" = "dbnet"
+  }
+  ipam_config {
+  	subnet = "172.20.0.0/16"
+  }
 }
 
 #iptables - Alpine Linux-based firewall container
@@ -29,13 +33,15 @@ resource "docker_container" "iptables" {
   name = "firewall"
   image = "${docker_image.iptables.master}"
   restart = "always"
-  network = "host"
-  env {
-    TCP_PORTS = "8080,3306"
-    HOSTS = "172.20.0.0/16"
+  networks_advanced { 
+  	name = "host"
   }
+  env = [
+  	"TCP_PORTS=8080,3306",
+  	"HOSTS=172.20.0.0/16"
+  ]
   capabilities {
-    add = "NET_ADMIN"
+    add = ["NET_ADMIN"]
   }
   command = ["/bin/ash", "iptables -A INPUT -i dbnet -j ACCEPT"]
 }
@@ -45,14 +51,16 @@ resource "docker_container" "mysql-server" {
   name = "mysql_server"
   image = "${docker_image.mysql-server.latest}"
   restart = "always"
-  network = "dbnet"
-  env {
-    MYSQL_ROOT_HOST = "%"
-    MYSQL_DATABASE = "db"
-    MYSQL_ROOT_PASSWORD = "root"
-    MYSQL_USER = "user1"
-    MYSQL_PASSWORD = "user1pass"
+  networks_advanced {
+  	name = "dbnet"
   }
+  env = [
+    "MYSQL_ROOT_HOST=%",
+    "MYSQL_DATABASE=db",
+    "MYSQL_ROOT_PASSWORD=root",
+    "MYSQL_USER=user1",
+    "MYSQL_PASSWORD=user1pass"
+  ]
   mounts {
     source = "mysql_db"
     target = "/var/lib/mysql"
@@ -69,12 +77,14 @@ resource "docker_container" "phpmyadmin" {
   name = "php_admin"
   image = "${docker_image.phpmyadmin.latest}"
   restart = "always"
-  network = "dbnet"
-  env {
-    PMA_HOST = "mysql_server"
+  networks_advanced {
+  	name = "dbnet"
   }
+  env = [
+    "PMA_HOST=mysql_server"
+  ]
   ports {
       internal = 8080
       external = 80
   }
-}  
+}
